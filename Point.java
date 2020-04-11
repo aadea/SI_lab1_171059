eb18caffbb52572fd9f5f2d8aa93bc6c3f8a7a8e
aadea <adea.kamberi@outlook.com>
2020-04-11 03:13:33 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -7,14 +7,73 @@ class Point {
 
 	//TODO constructor
 
+	public Point() {
+	}
+
+	public Point(String id, double x, double y, String color) {
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
 	//TODO setters and getters
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		if(xDirection=="L")
+		{
+			this.x-=1;
+		}
+		if(xDirection=="R")
+		{
+			this.x+=1;
+		}
+		if(yDirection=="U")
+		{
+			this.y+=1;
+		}
+		if(yDirection=="D")
+		{
+			this.y-=1;
+		}
 	}
 
 	public void draw () {
 		//TODO
+		System.out.println("x: "+x+" y: "+y);
 	}
 
 
